2b6f671c0b646d0eabbc73ba159bae7bf994a032
Nguyen Ha <nguyenha2651988@gmail.com>
2020-03-21 11:38:34 +0700

    ok

--- src/main/java/test_heroku/BlogPersonalApplication.java
+++ src/main/java/test_heroku/BlogPersonalApplication.java
@@ -12,12 +12,36 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import test_heroku.service.*;
 import test_heroku.service.impl.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BlogPersonalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogPersonalApplication.class, args);
+    }
+
+
+    @Configuration
+    public class CorsConfig  {
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("*")
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
+                            .allowedHeaders("X-Auth-Token", "Content-Type", "Authorization", "Content-Length", "X-Requested-With","accessToken")
+                            .allowCredentials(false)
+                            .maxAge(4800);
+                }
+            };
+        }
     }
 
     @Bean
